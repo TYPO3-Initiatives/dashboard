26375fdc9103b71fbcc797f31f181e21d273f72d
Richard Haeser <richard@maxserv.com>
2018-11-16 20:29:26 +0100

    Setup extension

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Dashboard',
+    'description' => 'Add dashboard features to TYPO3',
+    'category' => 'fe',
+    'author' => 'Richard Haeser',
+    'author_email' => 'richardhaeser@gmail.com',
+    'author_company' => '',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0 - 9.5.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
