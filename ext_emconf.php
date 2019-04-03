23b337ea47a725f53a7aa80327b5f4b5042b51b7
Richard Vollebregt <richard.vollebregt@maxserv.com>
2019-04-03 18:13:51 +0200

    [TASK] support v10 development versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0 - 9.5.99'
+            'typo3' => '8.7.0 - 10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
